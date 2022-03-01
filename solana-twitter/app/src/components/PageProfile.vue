a25cf0e9c8aa714a99d0683aff3031d0b267b6ef
Michał Flak <mflak@monadical.com>
2022-03-01 02:40:24 +0100

    Filtering pt2

--- solana-twitter/app/src/components/PageProfile.vue
+++ solana-twitter/app/src/components/PageProfile.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, watchEffect } from 'vue'
-import { fetchTweets } from '@/api'
+import { fetchTweets, authorFilter } from '@/api'
 import TweetForm from '@/components/TweetForm'
 import TweetList from '@/components/TweetList'
 import { useWorkspace } from '@/composables'
@@ -10,7 +10,8 @@ const loading = ref(true)
 const { wallet } = useWorkspace()
 
 watchEffect(() => {
-    fetchTweets()
+    if (! wallet.value) return
+    fetchTweets([authorFilter(wallet.value.publicKey.toBase58())])
         .then(fetchedTweets => tweets.value = fetchedTweets)
         .finally(() => loading.value = false)
 })
@@ -20,7 +21,7 @@ const addTweet = tweet => tweets.value.push(tweet)
 
 <template>
     <!-- TODO: Check connected wallet -->
-    <div v-if="true" class="border-b px-8 py-4 bg-gray-50">
+    <div v-if="wallet" class="border-b px-8 py-4 bg-gray-50">
         {{ wallet.publicKey.toBase58() }}
     </div>
     <tweet-form @added="addTweet"></tweet-form>
