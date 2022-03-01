b8fe91e3ec13a07cfdac8a9e5ba5e978e708ac26
Michał Flak <mflak@monadical.com>
2022-03-01 03:09:52 +0100

    tweeting

--- solana-twitter/app/src/composables/useWorkspace.js
+++ solana-twitter/app/src/composables/useWorkspace.js
@@ -5,14 +5,16 @@ import { Provider, Program } from '@project-serum/anchor'
 import idl from '../../../target/idl/solana_twitter.json'
 
 const programID = new PublicKey(idl.metadata.address)
+const preflightCommitment = 'processed'
+const commitment = 'processed'
 let workspace = null
 
 export const useWorkspace = () => workspace
 
 export const initWorkspace = () => {
     const wallet = useAnchorWallet()
-    const connection = new Connection('http://127.0.0.1:8899')
-    const provider = computed(() => new Provider(connection, wallet.value))
+    const connection = new Connection('http://127.0.0.1:8899', commitment)
+    const provider = computed(() => new Provider(connection, wallet.value, { preflightCommitment, commitment }))
     const program = computed(() => new Program(idl, programID, provider.value))
 
     workspace = {
